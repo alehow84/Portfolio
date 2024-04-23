e689206c0c94efe7a3b5ea8b39b3aed4addecd86
Alex Howlett <alex.howlett@live.co.uk>
2024-04-23 10:16:47 +0100

    begin planning SocialMEdia Component

---
+++ src/app/static/SocialMedia.tsx
@@ -0,0 +1,5 @@
+export default function SocialMedia({ size }: { size: number }) {}
+//create icons folder in public folder
+//save svg. files for social media icons in there
+//import Image into this file
+//make sure to render the SocialMedia Component in Home and Footer Components
