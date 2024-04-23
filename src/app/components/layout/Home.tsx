43a3ee4b4aeeeeec428a9cb601ed20e469df7b29
Alex Howlett <alex.howlett@live.co.uk>
2024-04-23 11:19:32 +0100

    last commit for SocialMedia

--- src/app/components/layout/Home.tsx
+++ src/app/components/layout/Home.tsx
@@ -3,7 +3,7 @@ import SocialMedia from "@/app/static/SocialMedia";
 export default function Home() {
   return (
     <>
-      <div className="flex items-center bg-gradient-to-r from-gradientLeft to-gradientRight h-screen">
+      <div className="flex flex-row items-center bg-gradient-to-r from-gradientLeft to-gradientRight h-screen">
         <h1>Hi, I'm Alex</h1>
         <h2>Random facts about me</h2>
         <p>Mother of cats</p>
