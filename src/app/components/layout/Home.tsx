c136d415f62e89669d9bf712ce28a4f43a5d2e3d
Alex Howlett <alex.howlett@live.co.uk>
2024-04-23 11:58:44 +0100

    amend text-size

--- src/app/components/layout/Home.tsx
+++ src/app/components/layout/Home.tsx
@@ -4,21 +4,21 @@ import SocialMedia from "@/app/static/SocialMedia";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-row items-center bg-gradient-to-r from-gradientLeft to-gradientRight h-screen">
-        <div className="flex-col">
+      <div className="flex flex-row justify-center items-center bg-gradient-to-r from-gradientLeft to-gradientRight h-screen">
+        <div className="flex-col flex-1 ">
           <Image
             src="/images/profile-pic.png"
             alt="photo of Alex Howlett"
             width={350}
             height={350}
-            className="rounded-full mt-20 mb-20"
+            className="flex rounded-full mt-20 mb-20"
           />
           <SocialMedia size={50} />
         </div>
-        <div className="flex-col">
+        <div className="flex-col flex-1 ">
           <h1 className="text-8xl">Hi, I'm Alex</h1>
           <p className="text-6xl">Random facts about me</p>
-          <p className="text-3xl">Mother of cats</p>
+          <p className="text-4xl">Mother of cats</p>
         </div>
       </div>
     </>
