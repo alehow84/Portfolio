9fd3b1f98a37c431c09f5c1512228ba08612572d
Alex Howlett <alex.howlett@live.co.uk>
2024-04-23 11:53:13 +0100

    amend Home text styling

--- src/app/components/layout/Home.tsx
+++ src/app/components/layout/Home.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import SocialMedia from "@/app/static/SocialMedia";
 
 export default function Home() {
@@ -5,12 +6,19 @@ export default function Home() {
     <>
       <div className="flex flex-row items-center bg-gradient-to-r from-gradientLeft to-gradientRight h-screen">
         <div className="flex-col">
-          <SocialMedia size={90} />
+          <Image
+            src="/images/profile-pic.png"
+            alt="photo of Alex Howlett"
+            width={350}
+            height={350}
+            className="rounded-full mt-20 mb-20"
+          />
+          <SocialMedia size={50} />
         </div>
         <div className="flex-col">
-          <h1>Hi, I'm Alex</h1>
-          <h2>Random facts about me</h2>
-          <p>Mother of cats</p>
+          <h1 className="text-8xl">Hi, I'm Alex</h1>
+          <p className="text-6xl">Random facts about me</p>
+          <p className="text-3xl">Mother of cats</p>
         </div>
       </div>
     </>
