ce755b3799643aba2a9106d67c5c5813aa39c760
Alex Howlett <alex.howlett@live.co.uk>
2024-04-23 11:29:26 +0100

    add flex columns to Home

--- src/app/components/layout/Home.tsx
+++ src/app/components/layout/Home.tsx
@@ -4,10 +4,14 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-row items-center bg-gradient-to-r from-gradientLeft to-gradientRight h-screen">
-        <h1>Hi, I'm Alex</h1>
-        <h2>Random facts about me</h2>
-        <p>Mother of cats</p>
-        <SocialMedia size={90} />
+        <div className="flex-col">
+          <SocialMedia size={90} />
+        </div>
+        <div className="flex-col">
+          <h1>Hi, I'm Alex</h1>
+          <h2>Random facts about me</h2>
+          <p>Mother of cats</p>
+        </div>
       </div>
     </>
   );
