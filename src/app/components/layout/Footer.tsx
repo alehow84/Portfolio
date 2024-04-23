165dd5f9886dc7f0837623a7dc2d5ebe69cfe41e
Alex Howlett <alex.howlett@live.co.uk>
2024-04-23 16:09:46 +0100

    amend footer padding

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -2,8 +2,10 @@ import SocialMedia from "@/app/static/SocialMedia";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center p-4 bg-gradient-to-r from-pink-700 to-gradientRight h-max text-white">
-      <SocialMedia size={30} />
+    <footer className="flex justify-center items-center pt-2 bg-gradient-to-r from-pink-700 to-gradientRight h-max text-white">
+      <div className="pt-2">
+        <SocialMedia size={20} />
+      </div>
     </footer>
   );
 }
