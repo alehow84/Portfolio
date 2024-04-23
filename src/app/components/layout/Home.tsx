c20219e8089228404d8c6ba812ab652232656fe3
Alex Howlett <alex.howlett@live.co.uk>
2024-04-23 11:14:05 +0100

    Add SocialMedia component - Email link incomplete

--- src/app/components/layout/Home.tsx
+++ src/app/components/layout/Home.tsx
@@ -1,10 +1,13 @@
+import SocialMedia from "@/app/static/SocialMedia";
+
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center bg-gradient-to-r from-gradientLeft to-gradientRight h-screen">
+      <div className="flex items-center bg-gradient-to-r from-gradientLeft to-gradientRight h-screen">
         <h1>Hi, I'm Alex</h1>
         <h2>Random facts about me</h2>
         <p>Mother of cats</p>
+        <SocialMedia size={90} />
       </div>
     </>
   );
