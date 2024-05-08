b5929d27a2cc7f3b92c9e2b3cbb8d38e984b085b
Alex Howlett <alex.howlett@live.co.uk>
2024-05-08 14:09:28 +0100

    improve text styling for mobile

--- src/app/components/layout/Skills.tsx
+++ src/app/components/layout/Skills.tsx
@@ -14,11 +14,11 @@ export default function Skills() {
         alt="transition"
       />
       <div className="flex flex-col justify-center items-center h-fit">
-        <h2 className="text-[2.25vw]">Skills</h2>
+        <h2 className="desktop:text-4xl mobile:text-3xl">Skills</h2>
         <div className="inline-flex items-center justify-center w-full">
-          <hr className="w-3/4 h-1 my-8 bg-orange-300 border-0 rounded" />
+          <hr className="w-3/4 h-[0.5vh] my-8 bg-orange-300 border-0 rounded" />
         </div>
-        <div className="p-16 text-[1vw]">
+        <div className="p-16 text-wrap desktop:text-md mobile:text-xs mobile:p-6">
           <SkillsList />
         </div>
       </div>
